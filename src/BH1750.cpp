5f55c59933ff8c1e3e6c752599d6cfc47ae8e758
Roni <roni.nuutinen@metropolia.fi>
2024-12-12 10:29:59 +0200

    style(als): Adhere to abstraction class' logic. Not working; i2c not responding.

--- src/BH1750.cpp
+++ src/BH1750.cpp
@@ -1,87 +1,86 @@
 #include "BH1750.hpp"
+
 #include <cstdio>
 
-BH1750::BH1750(const std::shared_ptr<PicoI2C> &i2c, BH1750::I2C_dev_addr i2c_dev_addr)
-        : m_i2c(i2c), m_dev_addr(i2c_dev_addr) {}
+#define CLIOUT printf
 
-bool BH1750::SetMode(BH1750::operation operation) {
-    switch (operation) {
-        case POWER_DOWN:
-        case POWER_ON:
-        case CONTINUOUS_HIGH_RES:
-        case CONTINUOUS_HIGH_RES_2:
-        case CONTINUOUS_LOW_RES:
-        case ONE_TIME_HIGH_RES:
-        case ONE_TIME_HIGH_RES_2:
-        case ONE_TIME_LOW_RES:
-            m_write_buffer[0] = operation;
-            m_i2c->write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_LEN);
-            m_mode = operation;
-            return true;
-        case RESET_MEASUREMENT_DATA:
-        case SET_MEASUREMENT_TIME_HIGH_BITS:
-        case SET_MEASUREMENT_TIME_LOW_BITS:
-            printf("Operation code %hhu does not set a mode.\n", operation);
-            break;
-        default:
-            printf("Undefined operation code %hhu.\n", operation);
-    }
-    return false;
+BH1750::BH1750(const std::shared_ptr<PicoW_I2C>& i2c, BH1750::I2C_dev_addr i2c_dev_addr)
+    : m_i2c(i2c)
+    , m_dev_addr(i2c_dev_addr)
+{
 }
 
-BH1750::operation BH1750::GetMode() {
+void BH1750::SetMode(BH1750::mode mode)
+{
+    if (GetMode() == mode) {
+        CLIOUT("Warning: Already in mode %hhu\n", mode);
+    }
+    m_write_buffer[0] = mode;
+    m_i2c->Write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_BUF_LEN);
+    m_mode = mode;
+    vTaskDelay(pdMS_TO_TICKS(10));
+}
+
+BH1750::mode BH1750::GetMode() const
+{
     return m_mode;
 }
 
-bool BH1750::ResetDataRegistry() {
-    if (m_mode != operation::POWER_DOWN) {
-        m_write_buffer[0] = operation::RESET_MEASUREMENT_DATA;
-        m_i2c->write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_LEN);
-        m_read_buffer.fill(0);
-        return true;
+void BH1750::Reset()
+{
+    auto const mode = GetMode();
+    if (mode == POWER_DOWN) {
+        SetMode(POWER_ON);
     }
-    printf("Cannot reset data while powered down.\n");
-    return false;
+    m_write_buffer[0] = RESET;
+    m_i2c->Write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_BUF_LEN);
+    vTaskDelay(pdMS_TO_TICKS(10));
+    if (mode == POWER_DOWN) {
+        SetMode(POWER_DOWN);
+    }
 }
 
-bool BH1750::SetMeasurementTime(uint8_t measurement_time_ms) {
+bool BH1750::SetMeasurementTimeMS(uint8_t measurement_time_ms)
+{
     if (MEASUREMENT_TIME_MIN <= measurement_time_ms && measurement_time_ms <= MEASUREMENT_TIME_MAX) {
-        m_write_buffer[0] = operation::SET_MEASUREMENT_TIME_HIGH_BITS | (measurement_time_ms & MEASUREMENT_TIME_HIGH_BITS);
-        m_i2c->write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_LEN);
-        m_write_buffer[0] = operation::SET_MEASUREMENT_TIME_LOW_BITS | (measurement_time_ms & MEASUREMENT_TIME_LOW_BITS);
-        m_i2c->write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_LEN);
+        m_write_buffer[0] = operation::SET_MTREG_HIGH_BITS | static_cast<uint8_t>(measurement_time_ms & MEASUREMENT_TIME_HIGH_BITS);
+        m_i2c->Write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_BUF_LEN);
+        vTaskDelay(pdMS_TO_TICKS(10));
+        m_write_buffer[0] = operation::SET_MTREG_LOW_BITS | static_cast<uint8_t>(measurement_time_ms & MEASUREMENT_TIME_LOW_BITS);
+        m_i2c->Write(m_dev_addr, m_write_buffer.data(), I2C_INSTRUCTION_BUF_LEN);
+        vTaskDelay(pdMS_TO_TICKS(10));
         m_measurement_time_ms = measurement_time_ms;
         return true;
+    } else {
+        CLIOUT("Measurement time [%hhu] outside accepted range (%hhu - %hhu).\n",
+            measurement_time_ms, MEASUREMENT_TIME_MIN, MEASUREMENT_TIME_MAX);
     }
-    printf("Measurement time [%hhu] outside accepted range (%hhu - %hhu).\n",
-           measurement_time_ms, MEASUREMENT_TIME_MIN, MEASUREMENT_TIME_MAX);
     return false;
 }
 
-float BH1750::ReadLuxData() {
-    return ConvertUint16ToLux(ReadMeasurementData());
-}
-
-float BH1750::GetLuxData() {
-    return ConvertUint16ToLux(GetMeasurementData());
-}
-
-uint16_t BH1750::ReadMeasurementData() {
-    if (m_i2c->read(m_dev_addr, m_read_buffer.data(), I2C_MEASUREMENT_LEN) > 0) {
-        m_read_buffer.fill(0);
-        return static_cast<uint16_t>(m_read_buffer[0]) << BYTE | m_read_buffer[1];
+float BH1750::ReadLuxData()
+{
+    auto lux = ConvertUint16ToLux(ReadMeasurementData());
+    if (m_mode == ONE_TIME_HIGH_RES || m_mode == ONE_TIME_HIGH_RES_2 || m_mode == ONE_TIME_LOW_RES) {
+        m_mode = POWER_DOWN;
     }
-    return ERROR_VALUE;
+    return lux;
 }
 
-uint16_t BH1750::GetMeasurementData() {
-    return static_cast<uint16_t>(m_read_buffer[0]) << BYTE | m_read_buffer[1];
+uint16_t BH1750::ReadMeasurementData()
+{
+    if (m_i2c->Read(m_dev_addr, m_read_buffer.data(), I2C_MEASUREMENT_BUF_LEN) > 0) {
+        printf("response\n");
+        return static_cast<uint16_t>((m_read_buffer[0]) << BYTE) | m_read_buffer[1];
+    }
+    return RESET_VALUE;
 }
 
-float BH1750::ConvertUint16ToLux(uint16_t u16) {
-    if (u16 != ERROR_VALUE) {
+float BH1750::ConvertUint16ToLux(uint16_t u16) const
+{
+    if (u16 != RESET_VALUE) {
         auto lux = static_cast<float>(u16);
-        const float measurement_time_factor = static_cast<float>(MEASUREMENT_TIME_DEFAULT) / static_cast<float>(m_measurement_time_ms);
+        float const measurement_time_factor = static_cast<float>(MEASUREMENT_TIME_DEFAULT) / static_cast<float>(m_measurement_time_ms);
         lux *= measurement_time_factor;
         float mode_factor = MODE_FACTOR_HIGH;
         if (m_mode == CONTINUOUS_HIGH_RES_2 || m_mode == ONE_TIME_HIGH_RES_2) {
@@ -93,5 +92,10 @@ float BH1750::ConvertUint16ToLux(uint16_t u16) {
         lux /= ACCURACY_FACTOR;
         return lux;
     }
-    return ERROR_VALUE;
+    return RESET_VALUE;
+}
+
+uint8_t BH1750::GetMeasurementTimeMs() const
+{
+    return m_measurement_time_ms;
 }
--- src/BH1750.hpp
+++ src/BH1750.hpp
@@ -1,70 +1,73 @@
 #pragma once
 
-#include "PicoI2C.h"
-#include "config.h"
 #include <array>
 #include <memory>
 
+#include "PicoW_I2C.h"
+#include "config.h"
+
 class BH1750 {
 public:
-
     enum I2C_dev_addr {
-        ADDR_LOW                   = 0x23,
-        ADDR_HIGH [[maybe_unused]] = 0x5C
+        ADDR_LOW = 0x23,
+        ADDR_HIGH = 0x5C
     };
 
+    static constexpr uint BAUDRATE_MAX = 400000;
+    static constexpr uint16_t RESET_VALUE = 0;
+
+protected:
     enum operation {
-        POWER_DOWN                     = 0b00000000,
-        POWER_ON                       = 0b00000001,
-        RESET_MEASUREMENT_DATA         = 0b00000111,
-
-        CONTINUOUS_HIGH_RES            = 0b00010000,
-        CONTINUOUS_HIGH_RES_2          = 0b00010001,
-        CONTINUOUS_LOW_RES             = 0b00010011,
-
-        ONE_TIME_HIGH_RES              = 0b00100000,
-        ONE_TIME_HIGH_RES_2            = 0b00100001,
-        ONE_TIME_LOW_RES               = 0b00100011,
-
-        SET_MEASUREMENT_TIME_HIGH_BITS = 0b01000000,
-        SET_MEASUREMENT_TIME_LOW_BITS  = 0b01100000
+        // clang-format off
+        RESET               = 0b00000111,
+        SET_MTREG_HIGH_BITS = 0b01000000,
+        SET_MTREG_LOW_BITS  = 0b01100000
+        // clang-format on
     };
 
-    explicit BH1750(const std::shared_ptr<PicoI2C>& picoI2C, BH1750::I2C_dev_addr i2c_dev_addr = ADDR_LOW);
-    bool SetMode(BH1750::operation operation);
-    BH1750::operation GetMode();
-    bool ResetDataRegistry();
-    bool SetMeasurementTime(uint8_t measurement_time_ms);
-    float ReadLuxData();
-    float GetLuxData();
+    enum mode {
+        // clang-format off
+        POWER_DOWN            = 0b00000000,
+        POWER_ON              = 0b00000001,
+        CONTINUOUS_HIGH_RES   = 0b00010000,
+        CONTINUOUS_HIGH_RES_2 = 0b00010001,
+        CONTINUOUS_LOW_RES    = 0b00010011,
+        ONE_TIME_HIGH_RES     = 0b00100000,
+        ONE_TIME_HIGH_RES_2   = 0b00100001,
+        ONE_TIME_LOW_RES      = 0b00100011
+        // clang-format on
+    };
 
-    static constexpr uint16_t ERROR_VALUE = 0;
+    explicit BH1750(const std::shared_ptr<PicoW_I2C>& picoI2C, BH1750::I2C_dev_addr i2c_dev_addr);
+    void SetMode(BH1750::mode mode);
+    BH1750::mode GetMode() const;
+    void Reset();
+    bool SetMeasurementTimeMS(uint8_t measurement_time_ms);
+    float ReadLuxData();
+    uint8_t GetMeasurementTimeMs() const;
 
 private:
-
     uint16_t ReadMeasurementData();
-    uint16_t GetMeasurementData();
-    float ConvertUint16ToLux(uint16_t u16);
+    float ConvertUint16ToLux(uint16_t u16) const;
 
-    static constexpr uint8_t I2C_INSTRUCTION_LEN = 1;
-    static constexpr uint8_t I2C_MEASUREMENT_LEN = 2;
-    static constexpr uint8_t MEASUREMENT_TIME_MIN     = 31;
+    static constexpr uint8_t I2C_INSTRUCTION_BUF_LEN = 1;
+    static constexpr uint8_t I2C_MEASUREMENT_BUF_LEN = 2;
+    static constexpr uint8_t MEASUREMENT_TIME_MIN = 31;
     static constexpr uint8_t MEASUREMENT_TIME_DEFAULT = 69; // nice
-    static constexpr uint8_t MEASUREMENT_TIME_MAX     = 254;
+    static constexpr uint8_t MEASUREMENT_TIME_MAX = 254;
     static constexpr uint8_t MEASUREMENT_TIME_HIGH_BITS = 0b11100000U;
-    static constexpr uint8_t MEASUREMENT_TIME_LOW_BITS  = 0b00011111U;
+    static constexpr uint8_t MEASUREMENT_TIME_LOW_BITS = 0b00011111U;
     static constexpr uint8_t BYTE = 8;
 
-    static constexpr float ACCURACY_FACTOR    = 1.2;
-    static constexpr float MODE_FACTOR_HIGH   = 1;
+    static constexpr float ACCURACY_FACTOR = 1.2;
+    static constexpr float MODE_FACTOR_HIGH = 1;
     static constexpr float MODE_FACTOR_HIGH_2 = 0.5;
-    static constexpr float MODE_FACTOR_LOW    = 4;
+    static constexpr float MODE_FACTOR_LOW = 4;
 
-    std::shared_ptr<PicoI2C> m_i2c;
+    std::shared_ptr<PicoW_I2C> m_i2c;
     BH1750::I2C_dev_addr m_dev_addr;
-    std::array<uint8_t, I2C_INSTRUCTION_LEN> m_write_buffer;
-    std::array<uint8_t, I2C_MEASUREMENT_LEN> m_read_buffer;
-    BH1750::operation m_mode{POWER_DOWN};
-    uint8_t m_measurement_time_ms{MEASUREMENT_TIME_DEFAULT};
-
+    std::array<uint8_t, I2C_INSTRUCTION_BUF_LEN> m_write_buffer;
+    std::array<uint8_t, I2C_MEASUREMENT_BUF_LEN> m_read_buffer;
+    BH1750::mode m_mode { POWER_DOWN };
+    uint8_t m_measurement_time_ms { MEASUREMENT_TIME_DEFAULT };
 };
