40337f6d36e1c5c0ead735eb72b6bc81c50fad5c
Roni <roni.nuutinen@metropolia.fi>
2025-02-13 09:20:26 +0200

    feat(motor): Taskify Motor class. Implement RTOS:Primitives into Motor.

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -1,11 +1,10 @@
 #include "Motor.h"
 
-#include <cstdio>
-
 #include <FreeRTOS.h>
 #include <hardware/gpio.h>
 #include <task.h>
 
+#include "Logger.hpp"
 #include "config.h"
 
 constexpr bool DIRECTION_CW = true;
@@ -14,11 +13,19 @@ constexpr bool DIRECTION_CCW = false;
 constexpr uint STEP_HIGH_MS = 1;
 constexpr uint STEP_LOW_MS = 1;
 
-Motor::Motor(PinStep step, PinDirection direction, PinLimitCW limit_cw, PinLimitCCW limit_ccw)
+Motor::Motor(const Motor::Infrastructure& infra, const char* name, PinStep step, PinDirection direction, PinLimitCW limit_cw, PinLimitCCW limit_ccw)
     : m_pin_step(static_cast<uint>(step))
     , m_pin_direction(static_cast<uint>(direction))
     , m_pin_limit_cw(static_cast<uint>(limit_cw))
     , m_pin_limit_ccw(static_cast<uint>(limit_ccw))
+    , m_s_move_now(infra.move_now)
+    , m_s_measure_als_1(infra.measure_als_1)
+    , m_s_continous_lux_als_1(infra.continous_lux_als_1)
+    , m_s_measure_als_2(infra.measure_als_2)
+    , m_s_continous_lux_als_2(infra.continous_lux_als_2)
+    , m_v_latest_lux(infra.latest_lux)
+    , m_v_target_lux(infra.target_lux)
+    , m_v_belt_position(infra.belt_position)
 {
     gpio_init(m_pin_step);
     gpio_set_dir(m_pin_step, GPIO_OUT);
@@ -33,75 +40,147 @@ Motor::Motor(PinStep step, PinDirection direction, PinLimitCW limit_cw, PinLimit
     gpio_init(m_pin_limit_ccw);
     gpio_set_dir(m_pin_limit_ccw, GPIO_IN);
     gpio_pull_up(m_pin_limit_ccw);
+
+    if (xTaskCreate(
+            TASK_KONDOM(Motor, Task),
+            name, DEFAULT_TASK_STACK_SIZE * 2,
+            static_cast<void*>(this),
+            tskIDLE_PRIORITY + 3,
+            &m_handle)
+        == pdTRUE) {
+        Logger::Log("Created task [{}]", name);
+    } else {
+        Logger::Log("Error: Failed to created task [{}]", name);
+    }
 }
 
+/// OPEN
 bool Motor::IsCWLimitSwitchPressed() const
 {
     return !gpio_get(m_pin_limit_cw);
 }
 
+/// CLOSE
 bool Motor::IsCCWLimitSwitchPressed() const
 {
     return !gpio_get(m_pin_limit_ccw);
 }
 
+/// OPEN
 bool Motor::StepCW() // NOLINT(readability-make-member-function-const)
 {
     if (IsCWLimitSwitchPressed()) {
+        Logger::Log("Right limit reached");
         return false;
     }
+    xTaskDelayUntil(&m_step_finished, STEP_LOW_MS);
     gpio_put(m_pin_direction, DIRECTION_CW);
     gpio_put(m_pin_step, true);
     vTaskDelay(pdMS_TO_TICKS(STEP_HIGH_MS));
     gpio_put(m_pin_step, false);
-    vTaskDelay(pdMS_TO_TICKS(STEP_LOW_MS));
+    m_step_finished = xTaskGetTickCount();
+    ++m_belt_position;
     return true;
 }
 
+/// CLOSE
 bool Motor::StepCCW() // NOLINT(readability-make-member-function-const)
 {
     if (IsCCWLimitSwitchPressed()) {
+        Logger::Log("Left limit reached");
         return false;
     }
+    xTaskDelayUntil(&m_step_finished, STEP_LOW_MS);
     gpio_put(m_pin_direction, DIRECTION_CCW);
     gpio_put(m_pin_step, true);
     vTaskDelay(pdMS_TO_TICKS(STEP_HIGH_MS));
     gpio_put(m_pin_step, false);
-    vTaskDelay(pdMS_TO_TICKS(STEP_LOW_MS));
+    m_step_finished = xTaskGetTickCount();
+    --m_belt_position;
     return true;
 }
 
-uint Motor::Calibrate()
+bool Motor::Calibrate()
 {
-    while (StepCW()) { }
-    uint step_count = 0;
+    Logger::Log("Calibrating...");
+    while (StepCW()) {
+        if (++m_belt_max > EXPECTED_MAX_STEPS_ONE_WAY) {
+            return false;
+        }
+    }
+    vTaskDelay(pdMS_TO_TICKS(DIRECTION_CHANGE_DELAY_TICKS));
+    m_belt_max = 0;
     while (StepCCW()) {
-        step_count += 1;
+        if (++m_belt_max > EXPECTED_MAX_STEPS_ONE_WAY) {
+            return false;
+        }
     }
-    return step_count;
+    m_belt_position = m_belt_max;
+    return true;
 }
 
-constexpr auto PIN_STEP = Motor::PinStep(10);
-constexpr auto PIN_DIRECTION = Motor::PinDirection(11);
-constexpr auto PIN_LIMIT_SWITCH_CW = Motor::PinLimitCW(12);
-constexpr auto PIN_LIMIT_SWITCH_CCW = Motor::PinLimitCCW(13);
-
-// TODO: remove
-static void test_motor_task([[maybe_unused]] void* param)
+bool Motor::OnTarget()
 {
-    Motor motor { PIN_STEP, PIN_DIRECTION, PIN_LIMIT_SWITCH_CW, PIN_LIMIT_SWITCH_CCW };
+    if (!m_v_latest_lux->Peek(&m_latest_lux, 0)) {
+        Logger::Log("Error: Latest Lux unavailable");
+        return true;
+    }
+    if (!m_v_target_lux->Peek(&m_target_lux, 0)) {
+        Logger::Log("Error: Target Lux unavailable");
+        return true;
+    }
+    m_current_lux_difference = (m_target_lux - m_latest_lux) / m_target_lux;
+    if (IsCWLimitSwitchPressed() && m_current_lux_difference < 0) {
+        return true;
+    }
+    if (IsCCWLimitSwitchPressed() && m_current_lux_difference > 0) {
+        return true;
+    }
+    static const float MARGIN = 0.1;
+    return std::abs(m_current_lux_difference) < MARGIN;
+}
 
-    uint steps = motor.Calibrate();
-    printf("Calibration done: %u steps\n", steps); // NOLINT(hicpp-vararg,cppcoreguidelines-pro-type-vararg)
+void Motor::AdjustCurtain()
+{
+    if (OnTarget()) {
+        return;
+    }
+    m_s_continous_lux_als_1->Give();
+    m_s_measure_als_1->Give();
+    m_s_continous_lux_als_2->Give();
+    m_s_measure_als_2->Give();
+    while (!OnTarget()) {
+        if (m_current_lux_difference > 0) {
+            if (StepCW()) {
+                m_v_belt_position->Overwrite(static_cast<uint>(m_belt_position / m_belt_max * 1000));
+            } else {
+                vTaskDelay(DIRECTION_CHANGE_DELAY_TICKS);
+                break;
+            }
+        } else {
+            if (StepCCW()) {
+                m_v_belt_position->Overwrite(static_cast<uint>(m_belt_position / m_belt_max * 1000));
+            } else {
+                vTaskDelay(DIRECTION_CHANGE_DELAY_TICKS);
+                break;
+            }
+        }
+    }
+    m_s_continous_lux_als_1->Give();
+    m_s_continous_lux_als_2->Give();
+}
 
+void Motor::Task()
+{
+    Logger::Log("Initiated");
+    if (Calibrate()) {
+        Logger::Log("Calibrated beyond expected maximum [{}]. Suspending.", EXPECTED_MAX_STEPS_ONE_WAY);
+        vTaskSuspend(m_handle);
+    } else {
+        Logger::Log("Calibrated. Length: {} steps", m_belt_max);
+    }
     while (true) {
-        while (motor.StepCW()) { }
-        while (motor.StepCCW()) { }
+        m_s_move_now->Take(portMAX_DELAY);
+        AdjustCurtain();
     }
-}
-
-// TODO: remove
-void test_motor()
-{
-    xTaskCreate(test_motor_task, "Motor", DEFAULT_TASK_STACK_SIZE, nullptr, tskIDLE_PRIORITY + 3, nullptr);
 }
--- src/Motor.h
+++ src/Motor.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include <FreeRTOS.h>
 #include <pico/stdlib.h>
+#include <task.h>
+
+#include "Queue.hpp"
+#include "Semaphore.hpp"
 
 // Assuming pinout
 //  1. Step
@@ -16,26 +21,68 @@
 
 class Motor {
 public:
+    struct Infrastructure {
+        RTOS::Semaphore* move_now;
+        RTOS::Semaphore* measure_als_1;
+        RTOS::Semaphore* continous_lux_als_1;
+        RTOS::Semaphore* measure_als_2;
+        RTOS::Semaphore* continous_lux_als_2;
+        RTOS::Variable<float>* latest_lux;
+        RTOS::Variable<float>* target_lux;
+        RTOS::Variable<uint16_t>* belt_position;
+    };
+
     enum class PinStep : uint {};
     enum class PinDirection : uint {};
     enum class PinLimitCW : uint {};
     enum class PinLimitCCW : uint {};
 
-    explicit Motor(PinStep step, PinDirection direction, PinLimitCW limit_cw, PinLimitCCW limit_ccw);
+    static constexpr auto PIN_STEP = Motor::PinStep(10);
+    static constexpr auto PIN_DIRECTION = Motor::PinDirection(11);
+    static constexpr auto PIN_LIMIT_SWITCH_CW = Motor::PinLimitCW(12);
+    static constexpr auto PIN_LIMIT_SWITCH_CCW = Motor::PinLimitCCW(13);
 
-    bool IsCWLimitSwitchPressed() const;
-    bool IsCCWLimitSwitchPressed() const;
+    explicit Motor(const Infrastructure& infra, const char* name, PinStep step, PinDirection direction, PinLimitCW limit_cw, PinLimitCCW limit_ccw);
+
+private:
+    [[nodiscard]] bool IsCWLimitSwitchPressed() const;
+    [[nodiscard]] bool IsCCWLimitSwitchPressed() const;
 
     bool StepCW();
     bool StepCCW();
+    bool Calibrate();
 
-    uint Calibrate();
+    bool OnTarget();
+    void AdjustCurtain();
 
-private:
+    void Task();
+
+    static constexpr uint EXPECTED_MAX_STEPS_ONE_WAY = 30'000;
+    static constexpr TickType_t DIRECTION_CHANGE_DELAY_TICKS = pdMS_TO_TICKS(10);
+
     uint m_pin_step;
     uint m_pin_direction;
     uint m_pin_limit_cw;
     uint m_pin_limit_ccw;
+
+    TickType_t m_step_finished = 0;
+
+    TaskHandle_t m_handle = nullptr;
+
+    RTOS::Semaphore* m_s_move_now;
+    RTOS::Semaphore* m_s_measure_als_1;
+    RTOS::Semaphore* m_s_continous_lux_als_1;
+    RTOS::Semaphore* m_s_measure_als_2;
+    RTOS::Semaphore* m_s_continous_lux_als_2;
+    RTOS::Variable<float>* m_v_latest_lux;
+    RTOS::Variable<float>* m_v_target_lux;
+    RTOS::Variable<uint16_t>* m_v_belt_position;
+
+    float m_latest_lux = 0;
+    float m_target_lux = 0;
+    float m_current_lux_difference = 0;
+    uint m_belt_position = 0;
+    uint m_belt_max = 0;
 };
 
 void test_motor();
